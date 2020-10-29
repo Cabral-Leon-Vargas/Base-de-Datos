0c3df9edfae6b87e0fb4d1859a72a3eca30220f7
MarcoJeanLeon <marco_jleont.ibm@upci.edu.mx>
2020-10-28 21:14:54 -0600

    Ultima corrección

--- LOGIN_practica/Practica_Login_Cabral_Leon_Vargas/registro.cs
+++ LOGIN_practica/Practica_Login_Cabral_Leon_Vargas/registro.cs
@@ -17,31 +17,29 @@ namespace Practica_Login_Cabral_Leon_Vargas
         {
             InitializeComponent();
         }
-        //aqui va la parte del codigo
-        private void registro_Load(object sender, EventArgs e)
+        private void Reg()
         {
-            private void Reg()
+            string connection = "datasource=localhost;port=3306;username=MarcoJeanLT;password=Mjean2*2=4;database=login;";
+            string query = "insert into log(`ID`,`usuario`, `contraseña`) VALUES (NULL,'" + reg_user_txt.Text + "', '" + reg_psw_txt.Text + "')";
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+
+            try
             {
-                string connection = "datasource=localhost;port=3306;username=MarcoJeanLT;password=Mjean2*2=4;database=login;";
-                string query = "insert into log(`ID`,`usuario`, `contraseña`) VALUES (NULL,'" + reg_user_txt.Text + "', '" + reg_psw_txt.Text + "')";
-                MySqlConnection conectionDatabase = new MySqlConnection(connection);
-                MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
-                databaseCommand.CommandTimeout = 60;
-
-                try
-                {
-                    conectionDatabase.Open();
-                    MySqlDataReader reader1 = databaseCommand.ExecuteReader();
-                    MessageBox.Show("Registro Exitoso \n Cierre esta ventana e inicie sesión", "HECHO");
-                    conectionDatabase.Close();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
+                conectionDatabase.Open();
+                MySqlDataReader reader1 = databaseCommand.ExecuteReader();
+                MessageBox.Show("Registro Exitoso \n Cierre esta ventana e inicie sesión", "HECHO");
+                conectionDatabase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
 
+        }
+        private void registro_Load(object sender, EventArgs e)
+        {
         }
 
         private void reg_user_txt_TextChanged(object sender, EventArgs e)
@@ -70,8 +68,7 @@ namespace Practica_Login_Cabral_Leon_Vargas
             }
             else
             {
-                // aqui va "Reg();"
-
+                Reg();
             }
         }
 
